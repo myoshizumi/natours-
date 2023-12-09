6073ed8596969ef3e25be637b67ee134af9674bf
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-09 17:52:30 +0900

    Serving Static Files

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 // 1) Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
     console.log('Hello from the middleware 🤟');
     next();
