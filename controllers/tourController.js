3d3b5a04a1ae727d505193cf6e0654c80275588e
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-10 16:56:18 +0900

    Making the API Better: Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,20 +5,28 @@ exports.getAllTours = async (req, res) => {
         console.log(req.query);
 
         // Build Query
-        // 1) Filtering
+        // 1A) Filtering
         const queryObj = { ...req.query };
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach((el) => delete queryObj[el]);
 
-        // 2)Advanced Filtering
+        // 1B)Advanced Filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(
             /\b(gte|gt|lte|lt)\b/g,
             (match) => `$${match}`,
         );
-        console.log(JSON.parse(queryStr));
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join('');
+            query = query.sort(sortBy);
+            // sort('price ratingsAverage')
+        } else {
+            query = query.sort('-createdAt');
+        }
 
         // Excute Query
         const tours = await query;
