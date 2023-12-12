0f4ad8e781ebe888f3ab1133cdc04a5d6aa2ee0d
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-12 22:38:47 +0900

    Making the API Better: Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,6 +28,14 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        // 3) Filed Filtering
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
         // Excute Query
         const tours = await query;
 
