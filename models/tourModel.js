6f9787ad25a633bcefc395dce722b0d540a441f7
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-16 15:18:25 +0900

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
             unique: true,
             trim: true,
         },
+        slug: String,
         duration: {
             type: Number,
             required: [true, 'A tour must have a duration'],
@@ -64,6 +66,21 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
+
+// Document Middlware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function (next) {
+//     console.log('Will save this document');
+//     next();
+// });
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
