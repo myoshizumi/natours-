11e62e53a8ba9ae56d010d30bb5d1341317682d0
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-23 18:06:09 +0900

    Protecting Tour Routes - Part 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-const signToken = (id) => {
-    jwt.sign({ id: id }, process.env.JWT_SECRET, {
+const signToken = function (id) {
+    return jwt.sign({ id: id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
 };
@@ -16,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
+        passwordChangedAt: req.body.passwordChangedAt,
     });
 
     const token = signToken(newUser._id);
@@ -42,7 +44,6 @@ exports.login = catchAsync(async (req, res, next) => {
     }
     // 3) if everuthing ok, send token to client
     const token = signToken(user._id);
-
     res.status(200).json({
         status: 'success',
         token,
@@ -58,7 +59,6 @@ exports.protect = catchAsync(async (req, res, next) => {
     ) {
         token = req.headers.authorization.split(' ')[1];
     }
-    console.log(token);
 
     if (!token) {
         return next(
@@ -69,10 +69,27 @@ exports.protect = catchAsync(async (req, res, next) => {
         );
     }
     // 2) Valification token
-
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
     // 3) Check if user still exists
-
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401,
+            ),
+        );
+    }
     // 4) Check if user changed password after the token was issued
-
+    if (currentUser.changesPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError(
+                'User recently changed password! Please log in again.',
+                401,
+            ),
+        );
+    }
+    // Grant access to protected route
+    req.user = currentUser;
     next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -33,6 +33,7 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords do not match',
         },
     },
+    passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -51,6 +52,18 @@ userSchema.methods.correctPassword = async function (
 ) {
     return await bcrypt.compare(candidatePassword, userPassword);
 };
+userSchema.methods.changesPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10,
+        );
+        return JWTTimestamp < changedTimestamp;
+    }
+    // false means not changed
+    return false;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
