c200097f9579c9217e02be666a5e7f8fee6243fd
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-23 12:53:28 +0900

    Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -23,8 +24,24 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
-        minlength: 8,
+        validate: {
+            // This only works on CREATE and SAVE!!!
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Passwords do not match',
+        },
     },
+});
+
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
