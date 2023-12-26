bbfba57facc7c65a230b691e4f5ee865c0060869
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-26 23:37:23 +0900

    Authorization: User Roles and Permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
         passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role,
     });
 
     const token = signToken(newUser._id);
@@ -93,3 +94,19 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = function (...roles) {
+    return (req, res, next) => {
+        // roles ['admin', 'lead-guide']. role ='user'
+        if (!roles.includes(req.user.role)) {
+            console.log(req.user.role);
+            return next(
+                new AppError(
+                    'You do not have permission to perform this action',
+                    403,
+                ),
+            );
+        }
+        next();
+    };
+};
