80515831be0e9a563dc839aaa39b8b33e72b0e0c
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-27 22:36:34 +0900

    Password Reset Functionality: Reset Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -110,3 +110,17 @@ exports.restrictTo = function (...roles) {
         next();
     };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    // 1) Get user based on POSTed email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return next(new AppError('There is no user with email address.', 404));
+    }
+    // 2) Generate the random reset
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+    // 3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
