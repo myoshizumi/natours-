d2e48f2c9472fe595d7e3e88c6917e5ece5080be
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-12-30 22:41:11 +0900

    Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,20 +12,28 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Global Middlewares
+// Set security HTTP headers
+app.use(helmet());
+//Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-
+// Limit requests from same API
 const limiter = rateLimit({
     max: 3,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
-
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(
+    express.json({
+        limit: '10kb',
+    }),
+);
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
-
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     // console.log(req.headers);
