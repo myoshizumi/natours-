a82547c6d05a1b530a3290a23f483c8f3f979fe4
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-02 12:55:25 +0900

    Building Handler Factory Functions: Delete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
     const reviews = await Review.find();
@@ -25,3 +26,5 @@ exports.createReview = catchAsync(async (req, res, next) => {
         },
     });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
