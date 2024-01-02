345156c941e1bc157428044961c356dad06365ec
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-02 13:34:20 +0900

    Factory Functions: Update and Create

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,12 +73,6 @@ exports.createUser = (req, res) => {
         message: 'This route is not implemented yet',
     });
 };
-
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not implemented yet',
-    });
-};
-
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
