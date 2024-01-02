cf2d11b4a0e41f23b49c263d9d26a86931ff6cc0
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-03 05:30:41 +0900

    Improving Read Performance with Indexes

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -75,6 +75,7 @@ exports.getAll = (Model) =>
             .limitFields()
             .paginate();
         const doc = await features.query;
+        // const doc = await features.query.explain();
         // Send Response
         res.status(200).json({
             status: 'success',
