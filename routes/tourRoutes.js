a7bb416bc03f061becbcad1e41bde5b0219f01b1
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-02 11:08:18 +0900

    Implementing Simple Nested Routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -26,6 +27,18 @@ router
         authController.protect,
         authController.restrictTo('admin', 'lead-guide'),
         tourController.deleteTour,
+    );
+
+// POST /tour/:id/reviews
+// GET /tour/:id/reviews
+// GET /tour/:id/reviews/:id
+
+router
+    .route('/:tourId/reviews')
+    .post(
+        authController.protect,
+        authController.restrictTo('user'),
+        reviewController.createReview,
     );
 
 module.exports = router;
