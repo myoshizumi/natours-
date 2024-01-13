e893d574117c2ea1ca1b861e51f417eb643dc7d2
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-13 15:32:28 +0900

    Updating User Data

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(
         limit: '10kb',
     }),
 );
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 // Data sanitazation against NoSQL query injection
 app.use(mongoSanitize());
