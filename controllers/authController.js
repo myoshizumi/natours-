31fabf133577f0963ed43ef2640b9c486c701c4d
myoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2024-01-14 15:45:36 +0900

    Sending Password Reset Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -178,18 +178,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     // 2) Generate the random reset
     const resetToken = user.createPasswordResetToken();
     await user.save({ validateBeforeSave: false });
-    // 3) Send it to user's email
-    const resetURL = `${req.protocol}://${req.get(
-        'host',
-    )}/api/v1/users/resetPassword/${resetToken}}`;
 
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \nIf you didn't forgot your password, please ignore this email. `;
+    // 3) Send it to user's email
     try {
-        // await sendEmail({
-        //     email: user.email,
-        //     subject: 'Your password reset token (valid for 10 min)',
-        //     message,
-        // });
+        const resetURL = `${req.protocol}://${req.get(
+            'host',
+        )}/api/v1/users/resetPassword/${resetToken}}`;
+
+        await new Email(user, resetURL).sendPasswordReset();
         res.status(200).json({
             status: 'success',
             message: 'Token sent to email!',
